bd063a94dcc95b8263bdc0e4fcebbeae05e13ceb
ilaria <ilarialan12@gmail.com>
2025-01-04 14:20:12 +0100

    aggiunta Sviluppatore

--- app/src/main/java/com/example/progetto_si/MainActivity.kt
+++ app/src/main/java/com/example/progetto_si/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.progetto_si
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.webkit.WebView
